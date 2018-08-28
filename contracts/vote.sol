a6e5fc770ffc271d5fcc3a1f303a530649e894cf
andynuma <c126001a@comwork.c.oka-pu.ac.jp>
2018-08-28 15:17:50 +0900

    symtax add

--- contracts/vote.sol
+++ contracts/vote.sol
@@ -4,7 +4,7 @@ import "./Owned.sol";
 
 contract vote is MyVerify {
 
-    mapping (uint => uint) voteId;
+    //mapping (uint => uint) voteId;
     mapping(uint => address) public voteToOwner;
     mapping(address => uint) ownerToVote;
     mapping(address => uint) ownerVoteCount;
@@ -15,25 +15,15 @@ contract vote is MyVerify {
 
     uint testId;
     
-    // struct Voter{
-    //     address voterAddr;
-    // }
-    
-    // Voter[] voters;
-    
-    address[] voterAddrs;
-    
     address voterAddr;
+    address organizerAddr;
     
     //vote 
     struct Vote{
-        bytes hashedVote;
-        //uint candidateId;
+        bytes32 hashedVote;
         address voterAddr;
-        address organizerAddr;
-        address inspectorAddr;
-        //string _signByOrganizer;
-        //string _signByInspector;
+        bytes signByOrganizer;
+        bytes signByInspector;
     }
     
     Vote[] public votes;
@@ -51,7 +41,6 @@ contract vote is MyVerify {
     //set voter address
     function setVoterAddr(address _voterAddr) public onlyOwner{
         voterAddr = _voterAddr;
-
         //voterAddrs[_index] = _voterAddr;
     }
     
@@ -59,54 +48,29 @@ contract vote is MyVerify {
         return count;
     }
     
-    //create vote
-    //hashしたIdを渡せば良い事に気づいた
-    function createVote(bytes _hashedVote) public onlyVoter returns(uint){
-        //最初はVoteのアドレス検証欄にはvoteAddrを入れておく.
-        //-1する意味あるのだろうか
-        //voteの
-        uint id = votes.push(Vote(_hashedVote,msg.sender,msg.sender,msg.sender))-1;
-        //voteToOwner[id] = msg.sender;
-        //ownerToVote[msg.sender] = id;
-        //ownerVoteCount[msg.sender]++;
+    function createVote(bytes32 _hashedVote) public onlyVoter returns(uint){
+        //票の初期化
+        uint id = votes.push(Vote(_hashedVote,msg.sender,"0x0","0x0"))-1;
+        //票と投票者の紐付け
+        voteToOwner[id] = msg.sender;
         return id;
     }
     
-    //hash voteId
-    
-    // function mykeccak256(uint _id) public returns(bytes32){
-    //     return keccak256("_id");
-    // }
-    
-    //(first)send vote to organizer
-    //使おうと思ったけど，結局idで管理すればok
-    // function sendVoteToOrganize(bytes32 _hashedId) public onlyVoter {
-    //     //add organizer hashedIdCollections
-    //     hashedIdCollections[msg.sender] = _hashedId;
-    // }
-    
-    //signature by organizer
-    function signByOrganizer(uint _voteId, bytes32 _hash, bytes _signature) public onlyOwner{
-        //send public key and vote
-        //signatureはクライアント側でweb3を使う必要あり
-        //web3.eth.sign(account, hash)のaccountをOrganaizerのアドレスで実行して
-        //_signatureに渡すこと
+    function signByOrganizer(uint _voteId, bytes _signature) public onlyOwner{
+        /*
+        signatureはクライアント側でweb3を使う必要あり
+        _signature = web3.eth.sign(account, hash)
+        */
         
         Vote storage myVote = votes[_voteId];
-        myVote.organizerAddr = ecverify(_hash, _signature);
+        myVote.signByOrganizer = _signature;
         //これでorganzierのアドレスが返して，それをVoteに入れる
         /////公開鍵を投票者に送信
         
     }
     
-    //send vote to onlyInspector
-    //多分これはいらない
-    // function sendVoteToInspector(bytes32 _hash, bytes _signature) public onlyInspector{
-    //     ecverify(_hash, _signature);
-    // }
-    
     //signature by Inspector
-    function signByInspector(uint _voteId, bytes32 _hash, bytes _signature) public onlyInspector{
+    function signByInspector(uint _voteId, bytes _signature) public onlyInspector{
         //send voter
         //send public key and vote
         //signatureはクライアント側でweb3を使う必要あり
@@ -114,31 +78,23 @@ contract vote is MyVerify {
         //_signatureに渡すこと
         
         Vote storage myVote = votes[_voteId];
-        myVote.inspectorAddr = ecverify(_hash, _signature);
-        //これでorganzierのアドレスが返して，それをVoteに入れる
+        //運営の署名の検証
+        require(organizerAddr == ecverify(myVote.hashedVote,myVote.signByOrganizer) );
+        //投票者の確認
+        //未実装
+        //署名
+        myVote.signByInspector = _signature;
     }
     
-    
-    //send signed vote to organizer
     function voteToCandidate(uint _voteId) public onlyVoter{
-        //verify
+        //Vote.organizerSigの内容をecverifyで確認する
         Vote storage myVote = votes[_voteId];
-        //この方法だと,myvote.organizerAddrにアドレスを設定設定しておけば票の票の偽装ができてしまう
-        //myVote.organizerSigにweb3.eth.singの結果を入れるようにs変更
-        //require(myVote.organizerAddr==0x0); //運営のアドレスを検査
-        //require(myVote.inspectorAddr==0x0); //監視者のアドレスを検査
-        //require(myVote.voterAddr == 0x0)　これは書かなくて良い（onlyVoterしているので）
-        //ここから公開鍵で暗号化された投票内容（hashではない）を復号して
-        
-        //候補者の票とする.
-        //復号はどうやってするか
+        require(inspectorAddr == ecverify(myVote.hashedVote, myVote.signByInspector));
+        //ここから公開鍵で暗号化された投票内容（hashではない）を復号
+        //hashedVoteを復号する.
         //candidateVoteCounts[_candidateId]++;
     }
 
-    //Inspection votes
-    function inspectionVote() public onlyInspector{
-        //sign and address is correct ?
-    }
 
     //count votes
     function getVote(uint _candidateId) public view onlyOwner returns(uint){
